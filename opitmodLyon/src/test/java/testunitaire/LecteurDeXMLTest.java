3b86194a26d2323c9540420b827754b649d133fb
SHU <a806621313@gmail.com>
2018-11-26 12:07:41 +0100

    test

---
+++ opitmodLyon/src/test/java/testunitaire/LecteurDeXMLTest.java
@@ -0,0 +1,48 @@
+package testunitaire;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+
+import org.junit.Test;
+
+import modele.Intersection;
+import modele.Plan;
+import modele.Troncon;
+import services.LecteurDeXML;
+
+public class LecteurDeXMLTest {
+	
+	@Test
+	public void testLecturePlanXML() {
+		
+		/*
+		//Given
+		Intersection intersection1=new Intersection(25175791,45.75406,4.857418);
+		Intersection intersection2=new Intersection(25175778,45.75343,4.8574653);
+		Intersection intersection3=new Intersection(2117622723,45.75425,4.8591485);
+		Troncon troncon1=new Troncon(25175778,69.979805,"Rue Danton",25175791);
+		Troncon troncon2=new Troncon(2117622723,136.00636,"Rue de l'Abondance",25175791);
+		HashMap<Long,Intersection> intersections = new HashMap<Long,Intersection>();
+		IdentityHashMap<Long,Troncon> troncons = new IdentityHashMap<Long,Troncon>();
+		intersections.put(intersection1.getId(), intersection1);
+		intersections.put(intersection2.getId(), intersection2);
+		intersections.put(intersection3.getId(), intersection3);
+		troncons.put(troncon1.getOrigine(), troncon1);
+		troncons.put(troncon2.getOrigine(), troncon2);
+		Plan planTest = new Plan(intersections,troncons,4.8591485,4.857418,45.75425,45.75343);
+		//When
+		//Plan planCode = lecteur.lecturePlanXML("fichiersXML2018/testPlan.xml");
+		Plan planCode = LecteurDeXML.getInstance().lecturePlanXML("fichiersXML2018/testPlan.xml");
+		
+		//Then
+		assertEquals(planTest,planCode);
+		*/
+		
+		
+		
+		
+	}
+
+}
